dda69f1fd6125f0dcd8b14d546c376d7ed8f1cca
sidharth9796 <58071713+sidharth9796@users.noreply.github.com>
2020-04-09 09:53:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,13 @@ import plotly.graph_objs as go
 import pandas as pd
 import os
 from random import randrange
-df_metrics = pd.read_csv('/home/drive/neuralcoref/bert_spacy_metrics.csv')
-df_count=pd.read_csv('/home/drive/neuralcoref/entity_count.csv')
+df_metrics = pd.read_csv('https://raw.githubusercontent.com/sidharth9796/plotly-dash-drive/master/bert_spacy_metrics.csv')
+df_count=pd.read_csv('https://raw.githubusercontent.com/sidharth9796/plotly-dash-drive/master/entity_count.csv')
 
-df_1 = pd.read_csv('/home/drive/neuralcoref/entity_count.csv')
-df_2 = pd.read_csv('/home/drive/neuralcoref/data_new.csv')
+df_1 = pd.read_csv('https://raw.githubusercontent.com/sidharth9796/plotly-dash-drive/master/entity_count.csv')
 
-df_model=pd.read_csv('/home/drive/neuralcoref/datarecords.csv')
-#df_summary=pd.read_csv('model_summary.csv')
+
+df_model=pd.read_csv('https://raw.githubusercontent.com/sidharth9796/plotly-dash-drive/master/datarecords.csv')
 
 external_stylesheets=[
     'https://codepen.io/chriddyp/pen/bWLwgP.css',
